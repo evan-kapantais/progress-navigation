a209df5cc5884fd7473e88b1619b9c910e439f35
evan-kapantais <evankapantais@gmail.com>
2019-03-19 08:55:47 +0100

    done list

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var liDesc = document.getElementById('desc');
 var liConfirm = document.getElementById('confirm');
 var textTarget = document.getElementsByClassName('text-target')[0];
 var btnTarget1 = document.getElementsByClassName('btn1')[0];
-var btnTarget1 = document.getElementsByClassName('btn2')[0];
+var btnTarget2 = document.getElementsByClassName('btn2')[0];
 
 liTitle.addEventListener('click', SetTitle);
 liDesc.addEventListener('click', SetDescription);
 liConfirm.addEventListener('click', SetConfirm);
 
 function SetTitle() {
-  textTarget.innerHTML = "Choose title content.";
+  textTarget.innerHTML = 'Choose title content.';
   liTitle.classList.add('active');
 
   if (liDesc.classList.contains('active')) {
@@ -20,22 +20,29 @@ function SetTitle() {
     }
 
     liDesc.classList.remove('active');
+    btnTarget1.innerText = 'Submit Title';
+    btnTarget2.style.display = 'none';
   }
 }
 
 function SetDescription() {
-  textTarget.innerHTML = "Choose description content."
+  textTarget.innerHTML = 'Choose description content.'
   liDesc.classList.add('active');
-  // liConfirm.classList.remove('disabled');
 
   if (liConfirm.classList.contains('active')) {
     liConfirm.classList.remove('active');
   }
+
+  btnTarget1.innerText = 'Back';
+  btnTarget2.style.display = 'inline-block';
+  btnTarget2.innerText = 'Submit Description';
 }
 
 function SetConfirm() {
   if (liDesc.classList.contains('active')) {
     liConfirm.classList.add('active');
-    textTarget.innerHTML = "Are you happy now?"
+    textTarget.innerText = 'Are you happy now?';
+    btnTarget1.innerText = 'No, go back';
+    btnTarget2.innerText = 'Yes, go ahead';
   }
 }
